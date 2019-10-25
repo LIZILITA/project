6c4a9620824e92f771e885936c4cb2b8d1feb5d3
lizi <1518734591@qq.com>
2019-10-25 23:33:37 +0800

    Add---Comments

--- index.js
+++ index.js
@@ -2,62 +2,46 @@
 const usernameInput = document.querySelector("#signup__username");
 const passwordInput = document.querySelector("#signup__password");
 const passwordConfirmInput = document.querySelector(
-  "#signup__password_confirm"
+    "#signup__password_confirm"
 );
 const submitButton = document.querySelector("button");
 const strengthMeter = document.querySelector("#password_strength_meter");
-const strengthMeterCode=["strength-0","strength-1","strength-2","strength-3","strength-4","strength-5"];
+const strengthMeterCode = ["strength-0", "strength-1", "strength-2", "strength-3", "strength-4", "strength-5"];
 
-// PART 1: Show a confirmation message when the user clicks the submit button
+//添加submit按钮点击事件
 submitButton.addEventListener("click", submitForm);
+//添加键盘up事件，回车提交，修改密码强度条，消除警告红框
 window.addEventListener("keyup", function (k) {
-  if(k.keyCode===13){
-    submitForm();
-  }else {
-    passwordInput.classList.remove("missing");
-    passwordConfirmInput.classList.remove("missing")
-    strengthMeter.className = strengthMeterCode[getPasswordStrengthFrom0To5(passwordInput.value)];
-  }
+    //回车提交
+    if (k.keyCode === 13) {
+        submitForm();
+    } else {
+        //消除警告红框
+        passwordInput.classList.remove("missing");
+        passwordConfirmInput.classList.remove("missing");
+        //修改密码强度条，
+        strengthMeter.className = strengthMeterCode[getPasswordStrengthFrom0To5(passwordInput.value)];
+    }
 });
+//提交事件
 function submitForm() {
-  /* YOUR CODE HERE */
-  if(passwordInput.value===""){
-    passwordInput.classList.add("missing");
-  }else
-    if(passwordConfirmInput.value===""){
-    passwordConfirmInput.classList.add("missing");
-    }else
-  if(passwordInput.value!==passwordConfirmInput.value){
-    window.alert("您的密码输入不一致，请检查一遍并重新输入");
-  }else {
-    window.alert("welcome!" + usernameInput.value + "!");
-  }
+    //检查输入是否为空
+    if (passwordInput.value === "") {
+        passwordInput.classList.add("missing");
+    } else if (passwordConfirmInput.value === "") {
+        passwordConfirmInput.classList.add("missing");
+    } else
+        /*
+        检查输入一致
+         */
+    if (passwordInput.value !== passwordConfirmInput.value) {
+        window.alert("您的密码输入不一致，请检查一遍并重新输入");
+    } else {
+        window.alert("welcome!" + usernameInput.value + "!");
+    }
 }
-/* YOUR CODE HERE */
-
-
-
-// PART 2: Check if the password and password confirmation values match
-
-/* YOUR CODE HERE (might not be necessary) */
-
-
-
-// PART 3: Submit the form on "enter" key press
-
-/* YOUR CODE HERE */
-
-
-
-// PART 4: Highlight missing fields in the form on invalid submission
-
-/* YOUR CODE HERE */
-
-
-
-// PART 5: Check password strength as the user types
+//根据密码长度检查密码安全强度
 function getPasswordStrengthFrom0To5(password) {
-  return Math.round((Math.min(12, password.length) / 12) * 5);
+    return Math.round((Math.min(12, password.length) / 12) * 5);
 }
 
-/* YOUR CODE HERE */
