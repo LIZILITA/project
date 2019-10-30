bea9304def1c6d13370f16785e3530cc133b3800
lizi <1518734591@qq.com>
2019-10-30 20:33:56 +0800

    add---enemy---for---10

--- CarGame/CarGame.js
+++ CarGame/CarGame.js
@@ -1,15 +1,14 @@
 // Game Constants
-const NUMBLOCKS = 25;
+const NUMBLOCKS = 40;
 const MAXSPEED = 40;
 const MINSPEED = 8;
-const FRAMERATE = 50;
+const FRAMERATE = 200;
 const CARSPRITES = [
     'url("Black_viper.png")',
     'url("Mini_van.png")',
     'url("Police.png")'
 ];
 const strengthMeterCode = ["strength-0", "strength-1", "strength-2", "strength-3", "strength-4", "strength-5"];
-const enemyName = ["enemy0", "enemy1", "enemy2", "enemy3", "enemy4", "enemy5", "enemy6", "enemy7", "enemy8", "enemy9"];
 //初始化数据，5血量，绑定body元素，设置生成车辆为0；
 var numLives = 5;
 var Body = document.getElementById("body");
@@ -29,7 +28,7 @@ var enemyList = Body.querySelectorAll("div");
 
 //给所有的div加上style
 function addEnemyStyle() {
-    for (let i = 0; i < enemyName.length; i++) {
+    for (let i = 0; i < enemyList.length; i++) {
         enemyList[i].classList.add("some_class");
         enemyList[i].setAttribute("data-attr", "b1");
         enemyList[i].setAttribute("style", "margin-left:" + genRandomNum(0, window.innerWidth).toString() + "px;" + "background-position:center;position:absolute;margin-top:40px;width:40px;height:90px; background-image:" + CARSPRITES[genRandomNum(0, 3)] + ";background-size: cover;");
@@ -46,12 +45,11 @@ myCar.setAttribute("style", "height:90px;width:40px;position:absolute;background
 Body.appendChild(myCar);
 
 //获取血条的元素
-var BloodBar = document.getElementsByName("HP");
-BloodBar.className="strength-3";
 
+setInterval(moveBlock,NUMBLOCKS);
 //把所有的enemy往下移动
 function moveBlock() {
-    for (let i = 0; i < enemyName.length; i++) {
+    for (let i = 0; i < enemyList.length; i++) {
         let Speed = Number(enemyList[i].getAttribute("data-speed"));
         let Top = Number(enemyList[i].style.marginTop.match(/\d+/g)[0]);
         enemyList[i].style.marginTop = (Top + Speed).toString() + "px";
@@ -60,17 +58,16 @@ function moveBlock() {
 
 //把出了屏幕的enemy重新调到屏幕上方
 function moveBack() {
-    for (let i = 0; i < enemyName.length; i++) {
+    for (let i = 0; i < enemyList.length; i++) {
         let enemyLocation = Number(enemyList[i].style.marginTop.match(/\d+/g)[0]);
         if (enemyLocation > window.innerHeight) {
             enemyList[i].style.marginTop = "-40px";
         }
     }
 }
-
 //检测主角车辆是否与其他车辆发生碰撞，并把发生碰撞的enemy调回到屏幕上方
 function detectCollide() {
-    for (let i = 0; i < enemyName.length; i++) {
+    for (let i = 0; i < enemyList.length; i++) {
         //得到车辆的坐标和鼠标的坐标
         let enemyLocationX = Number(enemyList[i].style.marginLeft.match(/\d+/g)[0]);
         let enemyLocationY = Number(enemyList[i].style.marginTop.match(/\d+/g)[0]);
@@ -85,16 +82,16 @@ function detectCollide() {
             //把车调回
             enemyList[i].style.marginTop = "-40px";
             //更改血条
-            BloodBar.class = strengthMeterCode[numLives];
+           document.getElementById("Password_strength_meter").classList.replace(strengthMeterCode[numLives+1],strengthMeterCode[numLives]);
         }
     }
     //如果血条为0就弹出窗口并重新加载界面设置血量和雪条
-    if (numLives === 0) {
+    if (numLives < 0) {
         alert("Game Over!");
         window.location.reload();
         numLives = 5;
         //更改血条
-        BloodBar.className = strengthMeterCode[numLives];
+        document.getElementById("Password_strength_meter").classList.replace(strengthMeterCode[numLives],strengthMeterCode[5]);
     }
 }
 
@@ -107,7 +104,6 @@ document.addEventListener("mousemove", function (e) {
 setInterval(playGame, FRAMERATE);
 
 function playGame() {
-    moveBlock();
     detectCollide();
     moveBack();
 }
